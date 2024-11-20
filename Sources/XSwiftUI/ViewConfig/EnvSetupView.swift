9b9931f4bcf4c2344cd0a6f6df1f3ecbf4428dfd
shawon1fb <shawon0187@gmail.com>
2024-11-20 22:21:31 +0600

    feat: Add EnvSetupView and InitialCardView for environment setup in SwiftUI
    - Introduced `EnvSetupView`:
      - A container view that injects environment values such as `mainWindowSize` and `safeAreaInsets`.
      - Utilizes `GeometryReader` to dynamically capture and provide size and safe area insets to child views.
    
    - Added custom environment keys:
      - `MainWindowSizeKey` to manage the size of the main window.
      - `SafeAreaInsetsKey` to handle safe area insets as an `EdgeInsets` value.
      - Extended `EnvironmentValues` to include `mainWindowSize` and `safeAreaInsets` properties.
    
    - Implemented `InitialCardView`:
      - Serves as a higher-order wrapper around `EnvSetupView` to streamline the initialization of views that depend on the custom environment setup.
    
    This enhancement simplifies the management of layout-dependent environment properties and promotes reusable view configurations.

---
+++ Sources/XSwiftUI/ViewConfig/EnvSetupView.swift
@@ -0,0 +1,71 @@
+//
+//  EnvSetupView.swift
+//  XSwiftUI
+//
+//  Created by shahanul on 11/20/24.
+//
+
+
+import Foundation
+import SwiftUI
+
+public struct EnvSetupView<Content: View>: View {
+  @ViewBuilder var content: Content
+
+  public init(@ViewBuilder content: @escaping () -> Content) {
+    self.content = content()
+  }
+
+  public var body: some View {
+    GeometryReader { value in
+      GeometryReader { proxy in
+        content
+          .environment(\.mainWindowSize, proxy.size)
+        // .environment(\.safeAreaInsets, proxy.safeAreaInsets)
+      }
+      .ignoresSafeArea()
+      .environment(\.safeAreaInsets, value.safeAreaInsets)
+    }
+  }
+}
+
+private struct MainWindowSizeKey: EnvironmentKey {
+  static let defaultValue: CGSize = .zero
+}
+
+extension EnvironmentValues {
+  public var mainWindowSize: CGSize {
+    get { self[MainWindowSizeKey.self] }
+    set { self[MainWindowSizeKey.self] = newValue }
+  }
+
+  public var safeAreaInsets: EdgeInsets {
+    get { self[SafeAreaInsetsKey.self] }
+    set { self[SafeAreaInsetsKey.self] = newValue }
+  }
+}
+
+//private struct SafeAreaInsetsKey: EnvironmentKey {
+//   static var defaultValue: EdgeInsets = .init()
+//}
+
+// Define SafeAreaInsetsKey as an EnvironmentKey with EdgeInsets default value
+private struct SafeAreaInsetsKey: EnvironmentKey {
+    static let defaultValue: EdgeInsets = EdgeInsets()
+}
+
+
+
+public struct InitialCardView<Content: View>: View {
+  @ViewBuilder var content: () -> Content
+
+  public var body: some View {
+    EnvSetupView {
+      content()
+    }
+  }
+
+  public init(@ViewBuilder content: @escaping () -> Content) {
+    self.content = content
+  }
+}
